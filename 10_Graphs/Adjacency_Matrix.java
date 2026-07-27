7fd1be226290c5129adf585a92c91c1ae933f0a1
Naveena Sri.S <naveenasri0713@gmail.com>
2026-07-27 11:37:14 +0530

    Implement adjacency matrix input for graph representation
    Added functionality to input the number of vertices and edges, and create an adjacency matrix based on user input.

--- 10_Graphs/Adjacency_Matrix.java
+++ 10_Graphs/Adjacency_Matrix.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
+
+
+public class graphs {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of vertices:");
+        int v= sc.nextInt();
+        System.out.println("Enter the no.of edges");
+        int ed = sc.nextInt();
+
+        int[][] mat=new int[v][v];
+
+        for(int i =0;i<ed;i++){
+            int u= sc.nextInt();
+            int w= sc.nextInt();
+            mat[u][w]=1;
+        }
+
+        for (int i=0;i<v;i++){
+            for(int j=0;j<v;j++){
+                System.out.print(mat[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
 
